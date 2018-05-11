e478bb84204ae16c95ecf907501d72756387592e
Pavel Skala <pavel.skala@ackee.cz>
2018-05-11 13:31:35 +0200

    Fix rotation and done clicks before image is loaded into the CropImageView

--- photo-chooser/src/main/java/cz/ackee/choosephoto/CropPhotoFragment.java
+++ photo-chooser/src/main/java/cz/ackee/choosephoto/CropPhotoFragment.java
@@ -1,25 +1,20 @@
 package cz.ackee.choosephoto;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.drawable.DrawableCompat;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 
 import com.isseiaoki.simplecropview.CropImageView;
@@ -29,7 +24,6 @@ import com.isseiaoki.simplecropview.callback.SaveCallback;
 import java.io.File;
 
 import cz.ackee.choosephoto.utils.UiUtils;
-import cz.ackee.choosephoto.R;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
@@ -50,6 +44,8 @@ public class CropPhotoFragment extends Fragment {
     CropImageView cropImageView;
     ProgressBar progressBar;
     FrameLayout content;
+    ImageButton rotateButton;
+    ImageButton doneButton;
 
     public static Bundle getArgs(String file) {
         Bundle args = new Bundle();
@@ -79,20 +75,24 @@ public class CropPhotoFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        cropImageView = (CropImageView) view.findViewById(R.id.img_crop);
-        progressBar = (ProgressBar) view.findViewById(android.R.id.progress);
-        content = (FrameLayout) view.findViewById(android.R.id.content);
+        cropImageView = view.findViewById(R.id.img_crop);
+        progressBar = view.findViewById(android.R.id.progress);
+        content = view.findViewById(android.R.id.content);
+        rotateButton = view.findViewById(R.id.btn_rotate);
+        doneButton = view.findViewById(R.id.btn_done);
 
         cropImageView.setOutputMaxSize(MAX_SIZE, MAX_SIZE);
 
-        view.findViewById(R.id.btn_rotate).setOnClickListener(new View.OnClickListener() {
+        rotateButton.setEnabled(false); // to prevent clicks when cropImageView is not yet populated with bitmap
+        rotateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 rotate();
             }
         });
 
-        view.findViewById(R.id.btn_done).setOnClickListener(new View.OnClickListener() {
+        doneButton.setEnabled(false); // to prevent clicks when cropImageView is not yet populated with bitmap
+        doneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 done();
@@ -128,6 +128,8 @@ public class CropPhotoFragment extends Fragment {
                         public void accept(Bitmap bitmap) {
                             if (cropImageView != null) {
                                 cropImageView.setImageBitmap(bitmap);
+                                rotateButton.setEnabled(true);
+                                doneButton.setEnabled(true);
                             }
                         }
                     }, new Consumer<Throwable>() {
@@ -138,10 +140,9 @@ public class CropPhotoFragment extends Fragment {
                     });
         }
 
-        ImageView imgDone = (ImageView) view.findViewById(R.id.btn_done);
-        Drawable wrapper = DrawableCompat.wrap(imgDone.getDrawable().mutate());
+        Drawable wrapper = DrawableCompat.wrap(doneButton.getDrawable().mutate());
         DrawableCompat.setTint(wrapper, getArguments().containsKey(COLOR_KEY) ? getArguments().getInt(COLOR_KEY) : UiUtils.getColorAttribute(getContext(), R.attr.colorAccent));
-        imgDone.setImageDrawable(wrapper);
+        doneButton.setImageDrawable(wrapper);
     }
 
     private String getImageFile() {
