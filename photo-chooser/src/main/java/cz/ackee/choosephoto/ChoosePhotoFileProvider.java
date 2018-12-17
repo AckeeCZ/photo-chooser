30aeb5967c827650ca231285c055833deebe10ed
David Khol <david.khol@ackee.cz>
2018-12-17 15:44:11 +0100

    Updated library version.

--- photo-chooser/src/main/java/cz/ackee/choosephoto/ChoosePhotoFileProvider.java
+++ photo-chooser/src/main/java/cz/ackee/choosephoto/ChoosePhotoFileProvider.java
@@ -4,7 +4,7 @@ import android.support.v4.content.FileProvider;
 
 /**
  * This file provider defines directory in which app can store photos. Referencing
- * multiple file providers in androidManifest where each define {@link FileProvider} as its
+ * multiple file providers in androidManifest where each define FileProvider as its
  * implementation class leads to compile errors. Simply subclassing and referencing the subclass
  * is enough to fix the problem.
  *
